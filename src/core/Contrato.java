b4062c4241417837c4caf611266743c4add7df72
reisbrunna <reiisbrunna@gmail.com>
2017-03-15 20:02:56 -0300

    Update Contrato.java

--- src/core/Contrato.java
+++ src/core/Contrato.java
@@ -53,54 +53,56 @@ public class Contrato {
    *
    * @param anuncio
    * @param cliente
+   //Método construtor da classe. Cria um contrato entre um usuário e um anúncio.
    */
-  //Método construtor da classe. Cria um contrato entre um usuário e um anúncio.
+  
   public Contrato(Anunciante anuncio, Usuario cliente) {
 
 	}
 
 	/**
-	 *  
+	 *  // Método que salva o contrato no banco de dados
 	 */
-	// Método que salva o contrato no banco de dados
 	public void salvar() {
 
 	}
 
 	/**
 	 *  
-   * @param pagamento
-   * @param pagamento
+         * @param pagamento
+         * @param pagamento
+         // Modifica o método que pagamento do contrato
 	 */
-	// Modifica o método que pagamento do contrato
 	public void definirPagamento(Pagamento pagamento) {
 
 	}
 
 	/**
 	 *  
-   * @param qualificacao
-   * @param qualificacao
+         * @param qualificacao
+         * @param qualificacao
+         // Método que qualifica um contrato de acordo com o serviço recebido
 	 */
-	// Método que qualifica um contrato de acordo com o serviço recebido
 	public void qualificar(Qualificacao qualificacao) {
 
 	}
 
   /**
-   *
-   * @param situacao
+    *
+    * @param situacao
+    // Método que atualiza status do contrato de acordo com o serviço prestado
    */
-  // Método que atualiza status do contrato de acordo com o serviço prestado	
+ 	
   public void atualizarStatus(Status situacao) {
 
 	}
 
 	/**
 	 *  
-   * @param situacao
+         * @param situacao
+   	 // Método que notifica contratante sobre o status do serviço
 	 */
-	// Método que notifica contratante sobre o status do serviço
+	
 	public void notificarContratante(Status situacao) {
 
 	}
