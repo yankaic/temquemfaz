3b4fd0baae9772192cc55deb1510354db11cb7ec
IagoGomes <hiago_pacheco12@hotmail.com>
2017-03-15 19:13:03 -0300

    Organizando o java doc

--- src/core/Anunciante.java
+++ src/core/Anunciante.java
@@ -3,7 +3,7 @@ package core;
 import java.util.Collection;
 
 /**
- *
+ * 
  * @author yanka
  */
 public class Anunciante extends Usuario {
@@ -15,38 +15,33 @@ public class Anunciante extends Usuario {
 	private Collection<Anuncio> anuncio;
 
 	/**
-	 *  
-   * @param anuncio
+	 * Método que cria um anúncio, e associa ao anunciante
+         * @param anuncio : Anuncio
 	 */ 
-	// Método que cria um anúncio, e associa ao anunciante
 	public void criarAnuncio(Anuncio anuncio) {
 
 	}
 
 	/**
-	 *  
-   * @param anuncio
-   * @param anuncio
+	 * Método que exclui um anúncio, e associa ao anuciante
+         * @param anuncio : Anuncio
 	 */
-	// Método que exclui um anúncio, e associa ao anuciante
 	public void excluirAnuncio(Anuncio anuncio) {
 
 	}
 
 	/**
-	 *  
-   * @param anuncio
+	 * Método que edita um anúncio, e associa ao anunciante
+         * @param anuncio : Anuncio
 	 */
-	// Método que edita um anúncio, e associa ao anunciante
 	public void editarAnuncio(Anuncio anuncio) {
 
 	}
 
   /**
-   *
-   * @param contrato
+   * Método que notifica serviço e associa ao contrato	
+   * @param contrato : Contrato
    */
-  // Método que notifica serviço e associa ao contrato	
   public void notificarServico(Contrato contrato) {
 
 	}
