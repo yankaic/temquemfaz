dafafb1f62727f274654f42e3a032c9d3a552f76
reisbrunna <reiisbrunna@gmail.com>
2017-03-15 19:25:51 -0300

    Update Anunciante.java

--- src/core/Anunciante.java
+++ src/core/Anunciante.java
@@ -14,39 +14,39 @@ public class Anunciante extends Usuario {
 
 	private Collection<Anuncio> anuncio;
 
-	/**
-	 *  
-   * @param anuncio
+	 /**
+	 * Método que cria um anúncio, e associa ao anunciante
+         * @param anuncio
 	 */ 
-	// Método que cria um anúncio, e associa ao anunciante
+	
 	public void criarAnuncio(Anuncio anuncio) {
 
 	}
 
 	/**
-	 *  
-   * @param anuncio
-   * @param anuncio
+	 * Método que exclui um anúncio, e associa ao anuciante
+	   * @param anuncio
+	   * @param anuncio
 	 */
-	// Método que exclui um anúncio, e associa ao anuciante
+	
 	public void excluirAnuncio(Anuncio anuncio) {
 
 	}
 
 	/**
-	 *  
-   * @param anuncio
+	 * Método que edita um anúncio, e associa ao anunciante
+         * @param anuncio
 	 */
-	// Método que edita um anúncio, e associa ao anunciante
+	
 	public void editarAnuncio(Anuncio anuncio) {
 
 	}
 
   /**
-   *
-   * @param contrato
+   *  Método que notifica serviço e associa ao contrato	
+   *  @param contrato
    */
-  // Método que notifica serviço e associa ao contrato	
+
   public void notificarServico(Contrato contrato) {
 
 	}
