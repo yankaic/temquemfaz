d72222772b1b966afcea1c6cb5bb4206a8d89f53
IagoGomes <hiago_pacheco12@hotmail.com>
2017-03-15 19:29:12 -0300

    finalizando contrato

--- src/core/Contrato.java
+++ src/core/Contrato.java
@@ -1,41 +1,41 @@
 package core;
 
 /**
- *
- * @author yanka
+ * Está classe é uma abstação de um contrato realizado entre um usuário qualquer
+ *   e um anúncio. 
  */
 public class Contrato {
 
 	private float valor;
 
   /**
-   *
+   * Enumerador que define os estados possíveis de um contrato, desde seu início ao final. 
    */ 
  
   public enum Status { 
 
       /**
-       *
+       * quando o contrato já foi finalizando, isso incluí o pagamente e qualificação
        */
       CONCLUIDO, 
 
       /**
-       *
+       * quando o cliente opta por pagamento via a plataforma, todavia, sua realização ainda não foi confirmada;
        */
       TRAMITANDO, 
 
       /**
-       *
+       *  quando o contrato foi cancelado, seja por parte do anunciante ou do cliente;
        */
       CANCELADO, 
 
       /**
-       *
+       * quando o contrato foi realizado pelo cliente, mas ainda não obteve confirmação do anunciante;
        */
       SOLICITADO, 
 
       /**
-       *
+       *quando o serviço contratado foi realizado, mas o cliente ou anunciante ainda não o qualificou. 
        */
       ESPERANDO_QUALIFICACAO};
 
@@ -50,57 +50,53 @@ public class Contrato {
 	private Qualificacao qualificacao;
 
   /**
-   *
-   * @param anuncio
-   * @param cliente
+   * Método construtor da classe. Cria um contrato entre um usuário e um anúncio.
+   * @param anuncio : Anuncio
+   * @param cliente : Usuario
    */
-  //Método construtor da classe. Cria um contrato entre um usuário e um anúncio.
   public Contrato(Anunciante anuncio, Usuario cliente) {
 
 	}
 
 	/**
-	 *  
+	 *   Método que salva o contrato no banco de dados
 	 */
-	// Método que salva o contrato no banco de dados
+	
 	public void salvar() {
 
 	}
 
 	/**
-	 *  
-   * @param pagamento
-   * @param pagamento
+	 * Modifica o método que pagamento do contrato 
+         * @param pagamento : Pagamento
 	 */
-	// Modifica o método que pagamento do contrato
 	public void definirPagamento(Pagamento pagamento) {
 
 	}
 
 	/**
-	 *  
-   * @param qualificacao
-   * @param qualificacao
+	 *  Método que qualifica um contrato de acordo com o serviço recebido
+         * @param qualificacao : Qualificacao
 	 */
-	// Método que qualifica um contrato de acordo com o serviço recebido
+	 
 	public void qualificar(Qualificacao qualificacao) {
 
 	}
 
   /**
-   *
-   * @param situacao
+   * Método que atualiza status do contrato de acordo com o serviço prestado	
+   * @param situacao : Status
    */
-  // Método que atualiza status do contrato de acordo com o serviço prestado	
+  
   public void atualizarStatus(Status situacao) {
 
 	}
 
 	/**
-	 *  
-   * @param situacao
+	 * Método que notifica contratante sobre o status do serviço 
+         * @param situacao : Status
 	 */
-	// Método que notifica contratante sobre o status do serviço
+	// 
 	public void notificarContratante(Status situacao) {
 
 	}
